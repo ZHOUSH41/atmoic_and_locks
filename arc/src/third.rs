d487206534ea78e52b15490b3be1a8e28714b654
ZHOUSH41 <624061566@qq.com>
2023-05-10 01:25:24 +0800

    arc impl

---
+++ arc/src/third.rs
@@ -0,0 +1,128 @@
+///! weak pointer中，clone和drop都需要两次的原子指令操作
+/// 现在来优化
+///
+use std::{
+    cell::UnsafeCell,
+    mem::ManuallyDrop,
+    ops::Deref,
+    ptr::NonNull,
+    sync::atomic::{fence, AtomicUsize, Ordering},
+};
+pub struct Arc<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+
+unsafe impl<T: Sync + Send> Send for Arc<T> {}
+unsafe impl<T: Sync + Send> Sync for Arc<T> {}
+
+pub struct Weak<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+unsafe impl<T: Sync + Send> Send for Weak<T> {}
+unsafe impl<T: Sync + Send> Sync for Weak<T> {}
+
+struct ArcData<T> {
+    /// Number of `Arc`s.
+    data_ref_count: AtomicUsize,
+    /// Number of `Weak`s, plus one if there are any `Arc`s.
+    alloc_ref_count: AtomicUsize,
+    /// The data. Dropped if there are only weak pointers left.
+    data: UnsafeCell<ManuallyDrop<T>>,
+}
+
+impl<T> Arc<T> {
+    pub fn new(data: T) -> Self {
+        Self {
+            ptr: NonNull::from(Box::leak(Box::from({
+                ArcData {
+                    data_ref_count: AtomicUsize::new(1),
+                    alloc_ref_count: AtomicUsize::new(1),
+                    data: UnsafeCell::new(ManuallyDrop::new(data)),
+                }
+            }))),
+        }
+    }
+
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+}
+
+impl<T> Deref for Arc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        // Safety: Since there's an Arc to the data,
+        // the data exists and may be shared.
+        unsafe { &*self.data().data.get() }
+    }
+}
+
+impl<T> Weak<T> {
+    pub fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+
+    pub fn upgrade(&self) -> Option<Arc<T>> {
+        let mut n = self.data().data_ref_count.load(Ordering::Relaxed);
+        loop {
+            if n == 0 {
+                return None;
+            }
+            assert!(n < usize::MAX);
+            if let Err(e) = self.data().data_ref_count.compare_exchange_weak(
+                n,
+                n + 1,
+                Ordering::Relaxed,
+                Ordering::Relaxed,
+            ) {
+                n = e;
+                continue;
+            }
+            return Some(Arc { ptr: self.ptr });
+        }
+    }
+}
+
+impl<T> Clone for Weak<T> {
+    fn clone(&self) -> Self {
+        if self.data().alloc_ref_count.fetch_add(1, Ordering::Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Weak { ptr: self.ptr }
+    }
+}
+
+impl<T> Drop for Weak<T> {
+    fn drop(&mut self) {
+        if self.data().alloc_ref_count.fetch_sub(1, Ordering::Relaxed) == 1 {
+            fence(Ordering::Acquire);
+            drop(unsafe { Box::from_raw(self.ptr.as_ptr()) })
+        }
+    }
+}
+
+impl<T> Clone for Arc<T> {
+    fn clone(&self) -> Self {
+        if self.data().data_ref_count.fetch_add(1, Ordering::Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Arc { ptr: self.ptr }
+    }
+}
+
+impl<T> Drop for Arc<T> {
+    fn drop(&mut self) {
+        if self.data().data_ref_count.fetch_sub(1, Ordering::Release) == 1 {
+            fence(Ordering::Acquire);
+            // Safety: The data reference counter is zero,
+            // so nothing will access the data anymore.
+            unsafe {
+                ManuallyDrop::drop(&mut *self.data().data.get());
+            }
+            // Now that there's no `Arc<T>`s left,
+            // drop the implicit weak pointer that represented all `Arc<T>`s.
+            drop(Weak { ptr: self.ptr });
+        }
+    }
+}
